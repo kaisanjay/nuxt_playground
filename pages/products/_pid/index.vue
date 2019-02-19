de86fb25652449f1ed3ef4919994317508cc6a28
cdp <cdp@cdps-MacBook-Pro-2.local>
2019-02-19 22:23:02 +0530

    simple routing part 1

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+            A single product
+        </h1>
+    </div>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>products page</h1>
+    </div>
+</template>
